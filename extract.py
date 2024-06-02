cbcf0ae1ce73fc34f436163a199ec01767710cfd
manzil.akbar@gmail.com <manzil.akbar@gmail.com>
2024-06-02 23:58:08 +0700

    TODO: UI

--- extract.py
+++ extract.py
@@ -2,27 +2,12 @@ import numpy as np
 from skimage.color import rgb2gray
 from skimage.feature import graycomatrix, graycoprops
 from skimage import io
-import os
 
 def extract_glcm_features(image_path):
-    props = ['contrast', 'homogeneity', 'energy', 'correlation'] # 4 fitur yang akan diekstrak
+    props = ['contrast', 'homogeneity', 'energy', 'correlation'] # 4 fitur yang akan diekstrak, sesuai jurnal
     image = io.imread(image_path)
     gray_img = rgb2gray(image) # Ubah citra ke format greyscale
     gray_img = np.uint8(gray_img * 255)
     glcm = graycomatrix(gray_img, distances=[1], angles=[0, np.pi/4, np.pi/2, 3*np.pi/4], symmetric=True, normed=True) # Kalkulasi GLCM
     features = {prop: graycoprops(glcm, prop).ravel() for prop in props} # Ekstraksi fitur dari GLCM
     return features
-
-def extract_from_dataset(directory):
-    feature_list = []
-    for label in os.listdir(directory):
-        label_dir = os.path.join(directory, label) # Buka tiap sub-folder dataset (normal, cataract, etc)
-        if os.path.isdir(label_dir):
-            for image_file in os.listdir(label_dir):
-                image_path = os.path.join(label_dir, image_file)
-                features = extract_glcm_features(image_path)
-                features['label'] = label # Melabeli fitur dengan tipe citra asal (normal, cataract, etc)
-                feature_list.append(features)
-    return feature_list
-
-features = extract_from_dataset('dataset')
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# UI / IO Image
+import joblib
+from extract import extract_glcm_features
+import pandas as pd
+
+# load model
+knn = joblib.load('model/knn_model.pkl')
+label_encoder = joblib.load('model/label_encoder.pkl')
+scaler = joblib.load('model/scaler.pkl')
+
+# extract fitur dari citra yang ingin diklasifikasi
+image_path = 'image_test/image1.png'
+image_features = extract_glcm_features(image_path)
+image_features_df = pd.DataFrame([image_features], columns=['contrast', 'homogeneity', 'energy', 'correlation'])
+
+# flatten dataframe, seperti pada file model
+def flatten_features(features_df: pd.DataFrame):
+    flattened = pd.DataFrame()
+    for col in features_df.columns:
+        flattened = pd.concat([flattened, pd.DataFrame(features_df[col].tolist(), columns=[f"{col}_{i}" for i in range(len(features_df[col][0]))])], axis=1)
+    return flattened
+
+new_features_flattened = flatten_features(image_features_df)
+new_features_scaled = scaler.transform(new_features_flattened)
+predicted_class_encoded = knn.predict(new_features_scaled)
+predicted_class = label_encoder.inverse_transform(predicted_class_encoded)
+
+print(f'Prediksi mata: {predicted_class[0]}')
